32235a8cfa813f857d3cf506c01884e5475c1a19
Matías Marcó del Pont <matiasmdelp@gmail.com>
2022-05-12 21:07:37 -0300

    Add options to constructor with type check and ConfigError if fails

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,19 @@
 const { expect } = require('chai')
 
-const { ConfigManager } = require('../lib')
+const { ConfigManager, ConfigError } = require('../lib')
 
 describe('ConfigManager', function () {
   before(function () {
     process.env.ICECREAM = 'CHOCOLATE'
+  })
+
+  describe('#constructor()', function () {
+    it('should raise a ConfigError if environment variable type doesn\'t match passed type', function () {
+      const options = {
+        ICECREAM: 'number'
+      }
+      expect(() => new ConfigManager(options)).to.throw(ConfigError, 'Expected ICECREAM environment variable to be of type number. Instead, a string was found.')
+    })
   })
 
   describe('#get()', function () {
